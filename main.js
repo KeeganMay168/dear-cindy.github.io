83e161b991ebe4e9b0e0e93dc72c8192ccea587f
Keegan May <78191106+KeeganMay168@users.noreply.github.com>
2024-04-11 12:27:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const imageThree = document.querySelector(".image-3");
 const imageFour = document.querySelector(".image-4");
 const btnYes = document.querySelector(".btn-yes");
 const btnNo = document.querySelector(".btn-no");
+const termsBox = document.querySelector(".terms-box");
+const termsText = document.querySelector(".terms-text");
+const btnAcc = document.querySelector(".acc-btn");
+const btnDec = document.querySelector(".dec-btn");
 
 function getRandomNumber(min, max) {
   // Calculate the random number between min and max (inclusive)
@@ -41,4 +45,5 @@ btnYes.addEventListener("click", (e) => {
   imageTwo.classList.remove("hide");
   imageThree.classList.remove("hide");
   imageFour.classList.remove("hide");
+  
 });
